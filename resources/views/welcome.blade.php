1e278dc9d870387fc2f6a689665ac5d88d50bbdb
GHOST-CESC <90934787+CH4RL5@users.noreply.github.com>
2025-12-01 19:37:31 -0600

    file welcome.blade changed
    cambio el fondo del archivo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,8 +16,7 @@
 </head>
 
 <body class="antialiased font-sans">
-    <div
-        class="min-h-screen bg-gray-50 text-black/50 dark:bg-black dark:text-white/50 selection:bg-[#FF2D20] selection:text-white">
+    <div class="min-h-screen bg-gray-50 text-black/50 dark:text-white/50 selection:bg-[#FF2D20] selection:text-white">
         <div
             class="relative min-h-screen flex flex-col items-center justify-center selection:bg-[#FF2D20] selection:text-white">
             <div class="relative w-full max-w-2xl px-6 lg:max-w-7xl">
