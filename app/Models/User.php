ed8e7a199f150fc0e49330e84a99fcf6cdbe431d
GHOST-CESC <90934787+CH4RL5@users.noreply.github.com>
2025-12-01 20:47:18 -0600

    se quito la implementacion de gmail
    se quita la implementación

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,7 +9,7 @@ use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Laravel\Sanctum\HasApiTokens;
 use App\Enums\UserRole;
-class User extends Authenticatable implements MustVerifyEmail
+class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
